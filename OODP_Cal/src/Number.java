7bc61244ac930de5cf4c90c80e55918332158d09
psy082 <psychic223@hgu.edu>
2017-06-16 19:55:05 +0900

    Update Number.java

--- OODP_Cal/src/Number.java
+++ OODP_Cal/src/Number.java
@@ -1,4 +1,8 @@
-
+/* Number Class is for storing every single number as Number Element
+ * This extends Element class and its type is integer or float
+ * This has int and float field, but has no setter or getter method 
+ * which is for only the child of this class has setter or getter method according to their type 
+ */
 public class Number extends Element{
 
 	protected int integer;
