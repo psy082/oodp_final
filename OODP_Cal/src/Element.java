66eaef6032177e8f36a64fd37f62174054c1bd27
psy082 <psychic223@hgu.edu>
2017-06-16 19:44:23 +0900

    Update Element.java

--- OODP_Cal/src/Element.java
+++ OODP_Cal/src/Element.java
@@ -1,6 +1,10 @@
-
+/* Element Class is element class for all of components of Calculator 
+ * Element Class can be extended to Number or Operator elements  
+ * This has enumerator type which determines type of element.
+ */ 
 public class Element{	
 
+	//Chillren of Elements are one of three types, OP, INT, FLOAT. 
 	protected enum Type {OP, INT, FLOAT};
 	protected Type type;
 	protected String value;
