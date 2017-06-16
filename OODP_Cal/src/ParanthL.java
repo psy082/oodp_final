74ab4f69737e8524db21034b43983c3ab207c5fa
psy082 <psychic223@hgu.edu>
2017-06-16 20:32:21 +0900

    Update ParanthL.java

--- OODP_Cal/src/ParanthL.java
+++ OODP_Cal/src/ParanthL.java
@@ -1,4 +1,6 @@
-
+/* ParanthL class extends Operator class and this is for storing left paranthesis 
+ * The purpose of this class is marking operation priority so, its operation method is null
+ */
 public class ParanthL extends Operator {
 	
 	public ParanthL(String _op){
