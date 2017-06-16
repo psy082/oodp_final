7c49f10da35540517f0e1070b0bcf668f06955af
psy082 <psychic223@hgu.edu>
2017-06-16 21:00:28 +0900

    Update Parser.java

--- OODP_Cal/src/Parser.java
+++ OODP_Cal/src/Parser.java
@@ -1,22 +1,29 @@
 import java.util.Stack;
-
-
+/* Parser class is for parsing infix expression and storing it into the stack
+ */
 public class Parser {
 	
 	private String input;
 	
 	private Stack<Element> infix;
 		
+	//Parser constructor stores expression and calls parsing method 
+	//Infix notation which is stored in stack is made by parsing method
 	Parser(String _input) {
 		infix = new Stack<Element>();
 		input = _input;
 		parsing();
 	}
-	
+	//parsing method reads every single inputs and stores into stack
 	public void parsing() {
 	
+		
+		//Is is imporatant that seperates numbers and operator in the process of parsing
+		//To do this, String number variable is used
 		String number = "";
 		
+		//If input is digit, digit is stored in number variable
+		//Also, if there is dot, dot is stored in number variable
 		for(int i = 0; i < input.length(); i++){
 			switch (input.charAt(i)){
 			
@@ -34,6 +41,10 @@ public class Parser {
 				number += input.charAt(i);
 				break;
 			}
+		        //before plus operator is stored in stack, push_number method is called to store a unit of number
+			//if there is paranthesis before plus operator, number variable is empty
+			//In this case, it makes error to call push_number 
+		        //so, only number is not empty push_number method is called to avoid error
 			case '+': {
 				if(number != ""){
 					push_number(number);
@@ -66,6 +77,8 @@ public class Parser {
 				infix.push(new Division("/"));
 				break;
 			}
+			//It can not be the cases number is before left paranthesis in expression
+			//So, only storing left paranthesis is done
 			case '(': {
 				infix.push(new ParanthL("("));
 				break;
@@ -78,6 +91,9 @@ public class Parser {
 				infix.push(new ParanthR(")"));
 				break;
 			}
+			//equal operator means that expression is finished 
+			//If there is number before the equal operator, number is stored in stack 
+			//Otherwise, breaks the loop
 			case '=': {
 				if(number != ""){
 					push_number(number);
@@ -92,6 +108,10 @@ public class Parser {
 		}
 	}
 	
+	//push_number method is to store a unit of number into the stack 
+	//if a unit of number has dot, it means that that number is floating point number
+	//so, in this case this number is stored in stack as FloatNumber instance
+	//In the other case, that number is stroed in stack as IntegerNumber instance
 	public void push_number(String number) {
 		if(number.contains(".")){
 			if(number.charAt(number.length() - 1) == '.')
@@ -109,9 +129,8 @@ public class Parser {
 		}
 	}
 	
-
+	//getInfix method returns infix stack
 	public Stack<Element> getInfix(){
 		return infix;
 	}
-	
 }
