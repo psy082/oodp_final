f40bc97c952259fcd3ad3af6b6446caf24a4cf2c
psy082 <psychic223@hgu.edu>
2017-06-16 20:13:43 +0900

    Update Operator.java

--- OODP_Cal/src/Operator.java
+++ OODP_Cal/src/Operator.java
@@ -1,4 +1,9 @@
-
+/* Operator Class is for storing every single operator to calculate
+ * This extends Element class and its type is OP and the type of every its children is also OP
+ * Operator class is abstract class which has abstract method, operation
+ * evaluate method is template method which defers calculation to its child
+ * Its child class completes operation method to calculate
+ */ 
 public abstract class Operator extends Element{
 	
 	protected int priority;
@@ -28,6 +33,7 @@ public abstract class Operator extends Element{
 		op = _op;
 	}
 	
+	//Evaluate method is template method which is completed in child of Operator Class, by overriding operation method
 	protected Number Evaluate(Number e1, Number e2)
 	{
 		Type re_type = resolve_type(e1, e2);
@@ -43,6 +49,7 @@ public abstract class Operator extends Element{
 		return result;
 	}
 	
+	//resolve_type method is for let evaluator know which type will be right, from the two of Number inputs
 	protected Type resolve_type(Number e1, Number e2){
 		if(e1.getType() ==Type.INT && e2.getType() ==Type.INT){
 			return Type.INT;
@@ -53,6 +60,7 @@ public abstract class Operator extends Element{
 			
 	}
 	
+	//operation method is hook method which is overriden in the child of Operator class
 	abstract public Number operation(Number _e1, Number _e2, Type _type);
 	
 	abstract public Number operation(Number _e, Type _type);
